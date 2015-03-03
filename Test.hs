64ac3870f50d17a31bfb0bf4f0a789868b704355
Artúr Poór <gombocarti@gmail.com>
2015-03-03 14:35:21 +0100

    added tests check is IO action to be able to print formatted messages check shows the actual and expected results when test fail

--- Test.hs
+++ Test.hs
@@ -1,18 +1,25 @@
 module Test where
 
-import qualified SqDeep as D (Value(..), run)
+import qualified SqDeep as D (Value(..), run, Wrap(..))
 import Sq hiding ((==))
+import Parser hiding (check)
+import Text.Parsec (parse)
 
 tests :: [(String, D.Value)]
-tests = [ ("{m <- modules, f <- functions m | f}", D.Seq . map D.Fun $ [a,b,f])
-        , ("{m <- modules, name m == \"m\", f <- functions m, name f == \"g\" | returns f}", D.Seq []) ]
+tests = [ ("{m <- modules, f <- functions m | f}", D.wrap [a,b,f])
+        , ("{m <- modules, name m == \"m\", f <- functions m, name f == \"g\" | returns f}", D.Seq [])
+        , ("{m <- modules, name m == \"m1\", f <- functions m, name f == \"g\" | returns f}", D.Seq [])
+        , ("{m <- modules, f <- functions m, exported f, arity f == 0 | f}", D.Seq [])
+        , ("{f <- functions atModule , c <- calls f | c}", D.wrap [b])
+        , ("{m <- modules, not (name m =~ \"^test\") | m}", D.wrap [m1,m2])
+        ]
 
-check :: String
+check :: IO ()
 check = run tests
     where 
-      run [] = "all tests passed"
-      run ((t,exp):ts) = case D.run t of
-                         Right res -> if res == exp
-                                      then run ts
-                                      else "failed test: " ++ t
-                         Left err -> "failed test: " ++ err
+      run [] = putStrLn "all tests passed"
+      run ((t,expected):ts) = case D.run t of
+                         Right actual -> if actual == expected
+                                         then run ts
+                                         else putStrLn $ "failed test: " ++ t ++ "\nexpected: " ++ show expected ++ "\nactual: " ++ show actual
+                         Left err -> putStrLn $ "failed test: " ++ t ++ "\nreason: " ++ err
