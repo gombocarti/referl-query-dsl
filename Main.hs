ac6aa66b3a76c4f9618abc7270c88bf25e29a925
Artúr Poór <gombocarti@gmail.com>
2015-03-16 23:08:12 +0100

    indentation print program usage when not started with 1 or 3 arguments

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import TypeCheck
 --import SqDeep
 import SqRefact
 import qualified Test
-import Text.Parsec (parse,runParser)
+import Text.Parsec (runParser)
 import Control.Monad.Reader
 import Control.Monad.Error
 import System.Environment (getArgs)
@@ -13,12 +13,13 @@ runQuery :: Query a ->  Database -> Maybe Arg -> IO (Either String a)
 runQuery q db arg = runErrorT (runReaderT (runReaderT q db) arg)
 
 run :: String -> Maybe Arg -> IO ()
-run query arg = case runchk query start [] of
-                  Right (q ::: _) -> do db <- initErl "haskell@localhost"
-                                        x <- runQuery (eval q [] >>= showValue) db arg
-                                        case x of
-                                          Right s  -> putStrLn s
-                                          Left err -> putStrLn ("error: " ++ err)
+run sq arg = case runchk sq start [] of
+                  Right (q ::: _) -> do
+                    db <- initErl "haskell@localhost"
+                    x <- runQuery (eval q [] >>= showValue) db arg
+                    case x of
+                      Right s  -> putStrLn s
+                      Left err -> putStrLn ("error: " ++ err)
                   Left err -> putStrLn $ "error: " ++ err
 
 runchk :: String -> QParser UQuery -> TEnv ->  Either String TUQuery
@@ -30,5 +31,6 @@ main :: IO ()
 main = do
   a <- getArgs
   case a of
-    [s] -> run s Nothing
+    [s]          -> run s Nothing
     [s,file,pos] -> run s (Just (file,read pos))
+    _            -> putStrLn "usage: Main query"
